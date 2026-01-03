13766d59c172b0ae2935f43f545d5732ff4e65f5
Adarshtiwari <adarshtime46@gmail.com>
2026-01-04 00:32:24 +0530

    filter product

--- store/views.py
+++ store/views.py
@@ -5,9 +5,16 @@ from .models.category import Category
 # Create your views here.
 
 def home(request):
-    Products= Product.get_all_products();
+    Products= None
     category= Category.get_all_categories();
     
+    categoryID= request.GET.get('category')
+    if categoryID:
+        Products= Product.get_all_products_by_categoryid(categoryID)
+    else:
+        Products= Product.get_all_products()
+
+
     data= {}
     data['Product']= Products
     data['categories']= category
